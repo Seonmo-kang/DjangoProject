7803274bc3e595f505fef7194c755a7564684f72
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-01-07 01:10:38 -0500

    add appFolder in INSTALLED_APPS
    adding appFolder.apps.ProjectjangoConfig into INSTALLED_APPS

--- DjangoProject_mvc/settings.py
+++ DjangoProject_mvc/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appFolder.apps.ProjectjangoConfig',
+    'appFolder.apps.ProjectjangoConfig', #appFolder is added in INSTALLED_APPS
 ]
 
 MIDDLEWARE = [
