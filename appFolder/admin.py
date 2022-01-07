58db25d5f2001ab3cad0fd51cd1710955df4125a
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-01-07 01:21:38 -0500

    register model in admin page
    Register model in admin page
    import db model in admin.py
    admin.site.register( db model name )

--- appFolder/admin.py
+++ appFolder/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Hospital,Info
 # Register your models here.
+admin.site.register(Info) # For managing the app, register the model in admin page.
+admin.site.register(Hospital) # For managing the app, register the model in admin page.
