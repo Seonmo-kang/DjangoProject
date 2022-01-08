0bb0efc5c6aa1b76d4822a677674d1c470eff4e4
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-01-08 00:23:57 -0500

    New view based on class
    from django.views.generic.base import TemplateView
    
    create 2 views based on class

--- appFolder/views.py
+++ appFolder/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse;
+from django.views.generic.base import TemplateView # Display templates
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello Django! This is Django project 1")
+# def infoForm(request):
+#     return HttpResponse("This is infoForm site ${0}".format(0))
+
+# Using TemplateView : class based view, not function based view
+class InfoFromView(TemplateView):
+    template_name = 'infoForm.html'
+
+class HopistalListView(TemplateView):
+    template_name = 'provider.html'
