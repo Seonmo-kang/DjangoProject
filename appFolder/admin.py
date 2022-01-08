a919c82211484be50a8dbc82e32999b139c50610
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-01-08 01:36:49 -0500

    add admin.ModelAdmin
    add class with admin.ModelAdmin

--- appFolder/admin.py
+++ appFolder/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Hospital,Info
-# Register your models here.
-admin.site.register(Info) # For managing the app, register the model in admin page.
+
+class InfoAdmin(admin.ModelAdmin):
+    list_display = ("info_index","info_dateOfBirth","info_zipCode","info_vaccine","info_vaccineType","info_consentAck")
+admin.site.register(Info, InfoAdmin) # For managing the app, register the model in admin page.
 admin.site.register(Hospital) # For managing the app, register the model in admin page.
