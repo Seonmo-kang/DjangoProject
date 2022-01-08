3762046738a0c08a97380b33f01b33b84a3cafb4
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-01-08 00:18:04 -0500

    Update appFolder/urls.py for new view
    Routing new view based on class
    * use as_view() method

--- appFolder/urls.py
+++ appFolder/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from. import views
+from . import views # views.py all
 
 urlpatterns = [
-    path('',views.index,name='index')   # path ( route, view, kwargs, name )
+    path('',views.index,name='index'),   # path ( route, view, kwargs, name )
                                         # route : url address
                                         # view : view
                                         # kwargs : deliveried argments
                                         # name : route's name
-
+    path('infoForm',views.InfoFromView.as_view(),name="InfoForm"), # class based View must have .as_view() method in url
+    path('provider',views.HopistalListView.as_view(),name="Provider")
 ]
