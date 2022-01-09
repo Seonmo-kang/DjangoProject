76948fa92e4b178f183ef1d6f5ba4a6c8655e6a5
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-01-09 13:51:06 -0500

    init tests.py
    Update tests files but execute it after DB have done.

--- appFolder/tests.py
+++ appFolder/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Info
 
-# Create your tests here.
+# After have database done.
+# class InfoFormModelTest(TestCase):
+#     def setUpClass(self):
+#         Info.objects.create(
+#             info_index=1,
+#             info_dateOfBirth= '1996/01/02',
+#             info_zipCode= 12345,
+#             info_vaccine= 'fd',
+#             info_vaccineType='Pfizer',
+#             info_consentAck='True'
+#         )
+#     def test_infoForm
