689b1dbe1900a6f498825e1285f6c44dac7dd546
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-01-08 19:41:14 -0500

    DataBase drop & recreate
    After alter the colum and add column.
    Error :
    django.db.utils.DataError: value too long for type character varying(2)
    Solved : drop the database and delete migrations files.
    recreate and migrations, migrate 0001

--- appFolder/models.py
+++ appFolder/models.py
@@ -5,14 +5,24 @@ class Info(models.Model):
     info_index = models.BigAutoField("indexNum",primary_key=True) # Auto incredent number
     info_dateOfBirth = models.DateField("date of birth",help_text="DD/MM/YY") # Date of Birth
     info_zipCode = models.IntegerField("zipcode") # Zipcode
-    info_vaccine = models.CharField("vaccine",max_length=20) # 1 : First Dose , 2 : Booster Dose, 3 : Additional Dose
+    FirstDose = 'fd'
+    BoosterDose = 'bd'
+    AdditionalDose = 'ad'
+    VACCINE_IN_CHOICES = [
+        (FirstDose,'First Dose'),
+        (BoosterDose, 'Booster Dose'),
+        (AdditionalDose, 'Additional Dose')
+    ]
+    info_vaccine = models.CharField("vaccine",max_length=20,choices=VACCINE_IN_CHOICES) # 1 : First Dose , 2 : Booster Dose, 3 : Additional Dose
     info_vaccineType = models.CharField("vaccine type",max_length=20) # VaccineType 1: Pfizer 2: Janssen
     info_consentAck = models.BooleanField("consent Ack") # consent Acknowledgement
 
 class Hospital(models.Model):
-    Hospital_name = models.CharField('Hospital name',max_length=100, primary_key=True)
-    Hospital_address1 = models.CharField('Hospital address1',max_length=100)
-    Hospital_address2 = models.CharField('Hospital address2',max_length=100)
+    Hospital_name = models.CharField('Hospital name',max_length=20, primary_key=True)
+    Hospital_address1 = models.CharField('Hospital address1',max_length=20)
+    Hospital_address2 = models.CharField('Hospital address2',max_length=200,blank=True) #Address2 is optional
     Hospital_city = models.CharField('Hospital city',max_length=20)
     Hospital_state = models.CharField('Hospital state',max_length=10)
+    Hospital_zipcode = models.IntegerField('Hospital zipcode', default=00000)   #Hospital zipcode
+    #Hospital_vaccineType = models.CharField
     # Hospital_vaccineType = models. # vaccine could be multi...
