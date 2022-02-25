700a0b28269bfeded90918eebe771195c05074fe
Seonmo-kang <66151730+Seonmo-kang@users.noreply.github.com>
2022-02-25 16:32:37 -0500

    Adding API path
    Adding API path

--- appFolder/urls.py
+++ appFolder/urls.py
@@ -1,5 +1,6 @@
 from . import views # views.py all
 from rest_framework import routers
+from django.urls import path
 
 router = routers.DefaultRouter()
 router.register('info', views.InfoView, 'info')
@@ -8,14 +9,19 @@ router.register('hospitalList', views.HospitalListView, 'hospitalList')
 # router.register('infoForm',views.InfoFromView.as_view,'infoForm')
 # router.register('provider',views.HopistalListView.as_view,'provider')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
 
-# urlpatterns = [
-#     path('',views.index,name='index'),   # path ( route, view, kwargs, name )
-#                                         # route : url address
-#                                         # view : view
-#                                         # kwargs : deliveried argments
-#                                         # name : route's name
-#     path('infoForm',views.InfoFromView.as_view(),name="infoForm"), # class based View must have .as_view() method in url
-#     path('provider',views.HopistalListView.as_view(),name="provider")
-# ]
+urlpatterns = [
+    # path('',views.index,name='index'),   # path ( route, view, kwargs, name )
+                                        # route : url address
+                                        # view : view
+                                        # kwargs : deliveried argments
+                                        # name : route's name
+    path('hospital/all',views.HospitalList.as_view(),name='HospitalList'),
+    path('hospitalDetail/<str:state>/',views.HospitalStateDetail.as_view(),name='HospitalStateDetail'),
+    path('hospitalDetail/zipcode/<int:code>/',views.HospitalZipcodeDetail.as_view(),name='HospitalZipcodeDetail'),
+    path('hospitalDetail/info/<int:hospital_id>/',views.HospitalDetail.as_view(),name='HospitalDetail'),
+
+    path('infoList',views.InfoList.as_view(),name='InfoList'),
+    path('infoDetail',views.InfoDetail.as_view(),name='InfoDetail')
+]
